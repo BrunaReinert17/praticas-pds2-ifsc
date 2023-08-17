00d51af42516497b8a1f4eec20f67a919da0926e
BrunaReinert17 <carolinereinertbruna@gmail.com>
2023-08-17 20:49:37 -0300

    Conexao com banco e codigo

---
+++ bancoPDS2.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS banco_teste;
+
+USE banco_teste;
+
+CREATE TABLE IF NOT EXISTS `animal` (
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `marca` VARCHAR(45) NOT NULL,
+  `especie` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`)
+);
